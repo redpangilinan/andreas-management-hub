e7b267876f3861bcea760d016364eb1f29950fd8
redpangilinan <redpangilinan715@gmail.com>
2023-02-12 03:29:07 +0800

    v1.0.0: Changed username to email in login

--- assets/php/login.php
+++ assets/php/login.php
@@ -3,29 +3,33 @@ include "connection.php";
 
 // Login process
 if (isset($_POST['login'])) {
-    $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
-    $sql = "SELECT account_id, account_type FROM tb_accounts WHERE BINARY username='$username' AND BINARY password='$password' limit 1";
+    $sql = "SELECT account_id, firstname, lastname, account_type FROM tb_accounts WHERE BINARY email='$email' AND BINARY password='$password' limit 1";
     $result = mysqli_query($conn, $sql);
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $account_id = $row['account_id'];
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
         $account_type = $row['account_type'];
     }
     // Set every session and cookies then login
     if (mysqli_num_rows($result) == 1) {
-        $_SESSION['username'] = $username;
+        // Set session values
+        $_SESSION['email'] = $email;
+        $_SESSION['fullname'] = "$firstname $lastname";
         $_SESSION['password'] = $password;
         $_SESSION['account_id'] = $account_id;
         $_SESSION['account_type'] = $account_type;
+
         // Change the cookies names and link accordingly
         setcookie("ID", $_SESSION['account_id'], time() + 86400, "/", "http://localhost/Projects/github/php-scrud-with-login-authentication");
-        setcookie("Username", $_SESSION['username'], time() + 86400, "/", "http://localhost/Projects/github/php-scrud-with-login-authentication");
+        setcookie("Email", $_SESSION['email'], time() + 86400, "/", "http://localhost/Projects/github/php-scrud-with-login-authentication");
         setcookie("Password", $_SESSION['password'], time() + 86400, "/", "http://localhost/Projects/github/php-scrud-with-login-authentication");
+
         header("Location: dashboard.php");
-        
     } else {
-        echo 'Your Username or Password is incorrect!';
+        echo 'Your Email or Password is incorrect!';
     }
     mysqli_close($conn);
 }
-?>
