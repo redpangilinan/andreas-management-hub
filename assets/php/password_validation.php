54f0f9aa3a5eef6b5c350361360459516dcdf1fe
redpangilinan <redpangilinan715@gmail.com>
2023-02-13 20:20:13 +0800

    v1.0.0: Turned password validation to a function for versatility

--- assets/php/password_validation.php
+++ assets/php/password_validation.php
@@ -1,12 +1,15 @@
 <?php
 // Validate password strength
-$uppercase = preg_match('@[A-Z]@', $password);
-$lowercase = preg_match('@[a-z]@', $password);
-$number    = preg_match('@[0-9]@', $password);
-$specialChars = preg_match('@[^\w]@', $password);
+function validatePassword($password)
+{
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
 
-if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 6 || strlen($password) > 64) {
-    $password_check = false;
-} else {
-    $password_check = true;
+    if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 6 || strlen($password) > 64) {
+        return false;
+    } else {
+        return true;
+    }
 }
