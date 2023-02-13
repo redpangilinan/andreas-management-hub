3473213ab35b3ef0933ae1e41f39761c48f90e13
redpangilinan <redpangilinan715@gmail.com>
2023-02-13 18:50:48 +0800

    v1.0.0: Added validation to password input

---
+++ assets/php/password_validation.php
@@ -0,0 +1,12 @@
+<?php
+// Validate password strength
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+
+if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 6 || strlen($password) > 64) {
+    $password_check = false;
+} else {
+    $password_check = true;
+}
