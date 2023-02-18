f420340ae1bef4569b4fbf1d538a040c20b73e34
redpangilinan <redpangilinan715@gmail.com>
2023-02-18 21:41:18 +0800

    Added products to sidebar

--- assets/php/extensions/sidebar.php
+++ assets/php/extensions/sidebar.php
@@ -12,7 +12,7 @@ if ($account_type == "User") {
         </ul>
     </div>
 <?php
-//Sidebar for admins
+    //Sidebar for admins
 } else {
 ?>
     <div class="sidebar bg-dark text-white">
@@ -20,7 +20,7 @@ if ($account_type == "User") {
             <li><a href="./dashboard.php" title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
             <li><a href="./profile.php" title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
             <li><a href="./accounts.php" title="Users"><i class="fa-solid fa-users fa-lg"></i></a></li>
-            <li><a href="./records.php" title="Records"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
+            <li><a href="./products.php" title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
             <li><a href="?logout" title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
         </ul>
     </div>
