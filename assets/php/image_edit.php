4e71feee93c495599f769fbcf1c2b964863961e3
redpangilinan <redpangilinan715@gmail.com>
2023-02-20 18:31:02 +0800

    Image edit on record updates php script

---
+++ assets/php/image_edit.php
@@ -0,0 +1,19 @@
+<?php
+$dir = "../../images/products/";
+$file_name = $_FILES['edit_image']['name'];
+$file_tmp_name = $_FILES['edit_image']['tmp_name'];
+$ext = array("jpg", "png", "jpeg", "bmp");
+$split = explode('.', $file_name);
+$image_ext = strtolower(end($split));
+
+if (empty($_FILES['edit_image']['tmp_name'])) {
+    $file_name = 'default.jpg';
+} else {
+    if (in_array($image_ext, $ext)) {
+        $file_name = uniqid() . "-" . time() . "-" . date("Y-m-d") . "." . $image_ext;
+        move_uploaded_file($file_tmp_name, "$dir" . $file_name);
+    } else {
+        $file_name = 'default.jpg';
+        echo "no image ";
+    }
+}
