a465a8b8bdf815bcedac4ebba7c654d9511cfe30
redpangilinan <redpangilinan715@gmail.com>
2023-02-24 23:15:25 +0800

    Moved the product quantity code to a standalone file

--- assets/js/ajax/products_data.js
+++ assets/js/ajax/products_data.js
@@ -154,17 +154,3 @@ const editBtnEnable = () => {
     document.querySelector("#editButton").innerHTML = "Save changes";
     document.querySelector("#editButton").disabled = false;
 }
-
-//cart quantity
-$(document).ready(function(){
-		    $('.count').prop('disabled', true);
-   			$(document).on('click','.plus',function(){
-				$('.count').val(parseInt($('.count').val()) + 1 );
-    		});
-        	$(document).on('click','.minus',function(){
-    			$('.count').val(parseInt($('.count').val()) - 1 );
-    				if ($('.count').val() == 0) {
-						$('.count').val(1);
-					}
-    	    	});
- 		});
