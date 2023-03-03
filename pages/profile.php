21331d8e2826e62ccf57d5f7fc1a129b065dc972
redpangilinan <redpangilinan715@gmail.com>
2023-03-03 19:56:54 +0800

    Added skeleton loader to profile

--- pages/profile.php
+++ pages/profile.php
@@ -36,8 +36,19 @@ validateSession();
                         <div class="row d-flex justify-content-center align-items-center h-100">
                             <div class="col-md-12 col-xl-4">
                                 <div class="card" style="border-radius: 15px;">
-                                    <div class="card-body text-center" id="profile_info">
-                                        <p>Loading...</p>
+                                    <div class="card-body d-flex flex-column justify-content-center" id="profile_info">
+                                        <!-- Placeholder Skeleton Div -->
+                                        <div class="mb-3 d-flex">
+                                            <div>
+                                                <div class="mt-3 mb-4 skeleton skeleton-img"></div>
+                                            </div>
+                                            <div>
+                                                <h4 class="mb-2 skeleton skeleton-text"></h4>
+                                                <p class="skeleton skeleton-text"></p>
+                                                <p class="skeleton skeleton-text"></p>
+                                            </div>
+                                        </div>
+                                        <div class="skeleton skeleton-btn"></div>
                                     </div>
                                 </div>
                             </div>
