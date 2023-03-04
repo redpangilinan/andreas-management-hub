2dba0aa41f92e3564f37c42f00836d07017a02bf
JDterrenal <johnderickramosterrenal@gmail.com>
2023-03-04 22:32:42 +0800

    modal for cart and index

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
-<?php
-session_start();
-?>
-
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./assets/css/style.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js"></script>
-	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<title>Andrea's Fresh and Greens</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Andrea's Fresh and Greens</title>
 </head>
 
 <body>
-	<header>
-		<div class="header-container">
+    <header>
+        <div class ="header-container">
 			<div class="header-image">
 				<div class="header-text">
 					<i class="fa fa-arrow-circle-left"></i>
@@ -26,16 +22,14 @@ session_start();
 				</div>
 			</div>
 		</div>
-	</header>
-	<main>
-		<div class="menu-container">
-			<center>
-				<h2>Our Menu</h2>
-			</center><br>
+    </header>
+    <main>
+        <div class="menu-container">
+			<center><h2>Our Menu</h2></center><br>
 			<hr>
 			<h3>Baked Sushi</h3>
 			<div class="product-container">
-				<div class="product">
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -43,8 +37,8 @@ session_start();
 						<h4>₱260</h4>
 					</div>
 				</div>
-
-				<div class="product">
+				
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -52,8 +46,8 @@ session_start();
 						<h4>₱360</h4>
 					</div>
 				</div>
-
-				<div class="product">
+				
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -65,7 +59,7 @@ session_start();
 			<hr>
 			<h3>FRESH KANI & SHRIMP SPRING ROLLS</h3>
 			<div class="product-container">
-				<div class="product">
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -73,8 +67,8 @@ session_start();
 						<h4>₱260</h4>
 					</div>
 				</div>
-
-				<div class="product">
+				
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -82,8 +76,8 @@ session_start();
 						<h4>₱360</h4>
 					</div>
 				</div>
-
-				<div class="product">
+				
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -91,8 +85,8 @@ session_start();
 						<h4>₱460</h4>
 					</div>
 				</div>
-
-				<div class="product">
+				
+				<div class="product" data-bs-toggle="modal" data-bs-target="#orderModal">
 					<img src="./assets/images/andreas background.jpg" alt="">
 					<div class="product-text">
 						<span>Andrea's Fresh and Greens</span>
@@ -102,7 +96,7 @@ session_start();
 				</div>
 			</div>
 			<hr>
-
+			
 		</div>
 		<div class="navi">
 			<div class="nav-item-con">
@@ -110,22 +104,63 @@ session_start();
 					<span class="fas fa-shopping-bag"></span>
 					<span>Order</span>
 				</div>
-
+				
 				<div class="nav-item">
 					<span class="fas fa-cart-plus"></span>
 					<span>Cart</span>
 				</div>
-
+				
 				<div class="nav-item">
 					<span class="fa fa-info-circle"></span>
 					<span>About</span>
 				</div>
 			</div>
 		</div>
-
-	</main>
-	<footer></footer>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+        
+    </main>
+  	<div class="modal fade cart" data-backdrop="static" id="orderModal" tabindex="-1" aria-labelledby="orderModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="orderModal">
+				<div class="modal-header">
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" data-keyboard="false" data-backdrop="false"></button>
+				</div>
+				<div class="modal-body">
+					<div class="info-img">
+						<div class="order-img"><img src="../assets/images/andreas background.jpg" alt=""></div>
+						<div class="order-info">
+							<h5>8x4.5 BAKED CALI SUSHI w/ Nori Sheet Pack</h5>
+							<span>Price:</span> <span>123</span>
+						</div>
+					</div>
+					<hr>
+					<div class="del-type">
+						<h6>Delivery Type:</h6>
+						<form action="#">
+						  <select id="delivery" name="delivery">
+							<span for="delivery">Delivery Type:</span>
+							<option value="volvo">Pick Up</option>
+							<option value="saab">Delivery</option>
+						  </select>
+						</form>
+					</div>
+					<hr>
+					<div class="ordr-quantity">
+						<h6>Quantity:</h6>
+						<div class="quantity">
+							<span class="minus">-</span>
+							<input role="textbox" type="number" class="count" name="quantity" value="1">
+							<span class="plus">+</span>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="ordr-butn">Buy Now</button>
+                  	<button type="button" class="ordr-butn">Add to Cart</button>
+				</div>
+			</div>
+		</div>
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
