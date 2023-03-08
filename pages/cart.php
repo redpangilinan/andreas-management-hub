2edf60082a24f90a202f70429bc3c06820db2069
JDterrenal <johnderickramosterrenal@gmail.com>
2023-03-08 18:17:04 +0800

    cart

--- pages/cart.php
+++ pages/cart.php
@@ -48,6 +48,21 @@ session_start();
 					</div>
 				</div>
 			</div>
+
+			<div class ="sub-total-con">
+					<div class="sub-right">
+						<h6>Sub Total:</h6>
+						<h6>Delivery Type:</h6>
+						<h6>Total Price:</h6>
+					</div>
+					<div class="sub-left">
+						<h6>123</h6>
+						<h6>123</h6>
+						<h6>123</h6>
+						<button type="button" data-bs-toggle="modal" data-bs-target="#orderModal">Check Out</button>
+					</div>
+					
+			</div>
 		</div>
 
 		</div>
@@ -71,6 +86,47 @@ session_start();
 		</div>
 
 	</main>
+	<div class="modal fade cart" id="orderModal" tabindex="-1" aria-labelledby="orderModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content orderModal">
+				<div class="modal-header">
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div class="info-img">
+						<div class="order-img"><img src="../assets/images/andreas background.jpg" alt=""></div>
+						<div class="order-info">
+							<h5>8x4.5 BAKED CALI SUSHI w/ Nori Sheet Pack</h5>
+							<span>Price:</span> <span>123</span>
+						</div>
+					</div>
+					<hr>
+					<div class="del-type">
+						<h6>Delivery Type:</h6>
+						<form action="#">
+							<select id="delivery" name="delivery">
+								<span for="delivery">Delivery Type:</span>
+								<option value="volvo">Pick Up</option>
+								<option value="saab">Delivery</option>
+							</select>
+						</form>
+					</div>
+					<hr>
+					<div class="ordr-quantity">
+						<h6>Quantity:</h6>
+						<div class="quantity">
+							<span class="minus">-</span>
+							<input role="textbox" type="number" class="count" name="quantity" value="1">
+							<span class="plus">+</span>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="ordr-butn">Buy Now</button>
+				</div>
+			</div>
+		</div>
+	</div>
 	<footer></footer>
 	<script src="../assets/js/cart.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
