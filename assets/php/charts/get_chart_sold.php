5a33df9f1c2607d836cc0c9974f21634ba906503
redpangilinan <redpangilinan715@gmail.com>
2023-03-26 21:55:53 +0800

    Massively improved completed orders chart filters
    - Now shows all the days within the week in "This Week"
    - Now shows all the days within the month in "This Month"
    - Now shows all months within the year in "This Year"
    - Now shows all years within the business in "All Time"

--- assets/php/charts/get_chart_sold.php
+++ assets/php/charts/get_chart_sold.php
@@ -6,40 +6,87 @@ $option = $_GET['option'];
 // Set the SQL query based on the selected option
 switch ($option) {
     case 'weekly':
-        $sql = "SELECT DATE(order_date_time)
-		AS order_date, COUNT(*)
-		AS num_orders
-		FROM tb_orders
-		WHERE YEARWEEK(order_date_time, 1) = YEARWEEK(NOW(), 1)
-        AND status = 'Complete'
-		GROUP BY DATE(order_date_time)";
+        $sql = "SELECT DAYNAME(calendar.date) AS order_date, COUNT(tb_orders.order_id) AS num_orders
+        FROM (
+          SELECT DATE(NOW() - INTERVAL weekday(NOW()) DAY + INTERVAL seq DAY) AS date
+          FROM seq_0_to_6
+        ) calendar
+        LEFT JOIN tb_orders ON DATE(tb_orders.order_date_time) = calendar.date
+          AND YEARWEEK(tb_orders.order_date_time, 1) = YEARWEEK(NOW(), 1)
+          AND tb_orders.status = 'Complete'
+        GROUP BY calendar.date";
         break;
     case 'monthly':
-        $sql = "SELECT DATE(order_date_time)
-		AS order_date, COUNT(*)
-		AS num_orders
-		FROM tb_orders
-		WHERE MONTH(order_date_time) = MONTH(NOW())
-		AND YEAR(order_date_time) = YEAR(NOW())
-        AND status = 'Complete'
-		GROUP BY DATE(order_date_time)";
+        $sql = "SELECT calendar.date AS order_date, COUNT(tb_orders.order_id) AS num_orders
+        FROM (
+          SELECT LAST_DAY(NOW() - INTERVAL 1 MONTH) + INTERVAL 1 DAY + INTERVAL seq DAY AS date
+          FROM seq_0_to_30
+          WHERE LAST_DAY(NOW() - INTERVAL 1 MONTH) + INTERVAL 1 DAY + INTERVAL seq DAY <= NOW()
+        ) calendar
+        LEFT JOIN tb_orders ON DATE(tb_orders.order_date_time) = calendar.date
+          AND MONTH(tb_orders.order_date_time) = MONTH(NOW())
+          AND YEAR(tb_orders.order_date_time) = YEAR(NOW())
+          AND tb_orders.status = 'Complete'
+        GROUP BY calendar.date";
         break;
     case 'yearly':
-        $sql = "SELECT DATE(order_date_time)
-		AS order_date, COUNT(*)
-		AS num_orders
-		FROM tb_orders
-		WHERE YEAR(order_date_time) = YEAR(NOW())
-        AND status = 'Complete'
-		GROUP BY DATE(order_date_time)";
+        $sql = "SELECT 'January' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 1 AND status = 'Complete'
+        UNION
+        SELECT 'February' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 2 AND status = 'Complete'
+        UNION
+        SELECT 'March' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 3 AND status = 'Complete'
+        UNION
+        SELECT 'April' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 4 AND status = 'Complete'
+        UNION
+        SELECT 'May' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 5 AND status = 'Complete'
+        UNION
+        SELECT 'June' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 6 AND status = 'Complete'
+        UNION
+        SELECT 'July' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 7 AND status = 'Complete'
+        UNION
+        SELECT 'August' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 8 AND status = 'Complete'
+        UNION
+        SELECT 'September' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 9 AND status = 'Complete'
+        UNION
+        SELECT 'October' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 10 AND status = 'Complete'
+        UNION
+        SELECT 'November' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 11 AND status = 'Complete'
+        UNION
+        SELECT 'December' AS order_date, COUNT(*) AS num_orders
+        FROM tb_orders
+        WHERE YEAR(order_date_time) = YEAR(NOW()) AND MONTH(order_date_time) = 12 AND status = 'Complete'";
         break;
     case 'all_time':
-        $sql = "SELECT DATE(order_date_time)
-        AS order_date, COUNT(*)
-        AS num_orders
+        $sql = "SELECT 
+        YEAR(order_date_time) AS order_date, 
+        COUNT(*) AS num_orders
         FROM tb_orders
         WHERE status = 'Complete'
-        GROUP BY DATE(order_date_time)";
+        GROUP BY YEAR(order_date_time)
+        HAVING COUNT(*) >= 1
+        ORDER BY order_date;";
         break;
     default:
         // Return an empty response if the option value is invalid
