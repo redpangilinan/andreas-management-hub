adcd75083ae84a8b15996ea177a19cea6a97ae14
redpangilinan <redpangilinan715@gmail.com>
2023-04-02 22:13:34 +0800

    Removed registration form in admin login

--- pages/admin.php
+++ pages/admin.php
@@ -30,51 +30,12 @@ autoLogin();
                     <input type="password" name="password" placeholder="Password" class="form-control">
                     <span class="text-center text-danger"><?php include "../assets/php/login.php" ?></span>
                     <button type="submit" class="btn btn-dark" name="login">Sign in</button>
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#addModal">Sign up now!</a>
                 </div>
             </div>
         </div>
     </form>
 
-    <!-- Sign Up -->
-    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="addModalLabel">Register an Account</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <!-- Sign Up Form -->
-                <form id="form_add">
-                    <div class="modal-body">
-                        <div class="mb-3 d-flex">
-                            <input type="text" class="form-control" id="firstname" placeholder="First Name" required>
-                            <input type="text" class="form-control ms-2" id="lastname" placeholder="Last Name" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="email" class="form-control" id="email" placeholder="Email" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="number" class="form-control" id="contact_no" placeholder="Contact No." required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control" id="password" placeholder="Password" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="add-confirm btn btn-primary">Sign up</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
     <script src="../assets/js/alerts.js"></script>
-    <script src="../assets/js/ajax/accounts_data.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 </body>
 
