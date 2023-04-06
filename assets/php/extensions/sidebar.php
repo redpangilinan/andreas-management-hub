cf54e85173b920216ef4fd1d8c8f1c2c6a6247c2
redpangilinan <redpangilinan715@gmail.com>
2023-04-06 16:31:06 +0800

    Sidebar tooltips implementation with bootstrap

--- assets/php/extensions/sidebar.php
+++ assets/php/extensions/sidebar.php
@@ -6,11 +6,11 @@ if ($account_type == "Admin") {
 ?>
     <div class="sidebar bg-dark text-white">
         <ul class="list-unstyled">
-            <li><a href="./dashboard.php" title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
-            <li><a href="./profile.php" title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
-            <li><a href="./products.php" title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
-            <li><a href="./orders.php" title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
-            <li><a href="?logout" title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
+            <li><a href="./dashboard.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
+            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
+            <li><a href="./products.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
+            <li><a href="./orders.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
+            <li><a href="?logout" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
         </ul>
     </div>
 <?php
@@ -19,12 +19,12 @@ if ($account_type == "Admin") {
 ?>
     <div class="sidebar bg-dark text-white">
         <ul class="list-unstyled">
-            <li><a href="./dashboard.php" title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
-            <li><a href="./profile.php" title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
-            <li><a href="./accounts.php" title="Users"><i class="fa-solid fa-users fa-lg"></i></a></li>
-            <li><a href="./products.php" title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
-            <li><a href="./orders.php" title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
-            <li><a href="?logout" title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
+            <li><a href="./dashboard.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
+            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
+            <li><a href="./accounts.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Accounts"><i class="fa-solid fa-users fa-lg"></i></a></li>
+            <li><a href="./products.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
+            <li><a href="./orders.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
+            <li><a href="?logout" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
         </ul>
     </div>
 <?php
