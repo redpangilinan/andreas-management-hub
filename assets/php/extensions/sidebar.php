8127689376e634ae66a8852e1312428bd94c814a
redpangilinan <redpangilinan715@gmail.com>
2023-04-06 17:14:20 +0800

    Updated the icons and added customers page in the sidebar

--- assets/php/extensions/sidebar.php
+++ assets/php/extensions/sidebar.php
@@ -7,7 +7,8 @@ if ($account_type == "Admin") {
     <div class="sidebar bg-dark text-white">
         <ul class="list-unstyled">
             <li><a href="./dashboard.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
-            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
+            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-address-card fa-lg"></i></a></li>
+            <li><a href="./customers.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Customers"><i class="fa-solid fa-users fa-lg"></i></a></li>
             <li><a href="./products.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
             <li><a href="./orders.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
             <li><a href="?logout" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
@@ -20,8 +21,9 @@ if ($account_type == "Admin") {
     <div class="sidebar bg-dark text-white">
         <ul class="list-unstyled">
             <li><a href="./dashboard.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard"><i class="fa-solid fa-desktop fa-lg"></i></a></li>
-            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-user fa-lg"></i></a></li>
-            <li><a href="./accounts.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Accounts"><i class="fa-solid fa-users fa-lg"></i></a></li>
+            <li><a href="./profile.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Profile"><i class="fa-solid fa-address-card fa-lg"></i></a></li>
+            <li><a href="./accounts.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Accounts"><i class="fa-solid fa-user fa-lg"></i></a></li>
+            <li><a href="./customers.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Customers"><i class="fa-solid fa-users fa-lg"></i></a></li>
             <li><a href="./products.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Products"><i class="fa-solid fa-table-list fa-lg"></i></a></li>
             <li><a href="./orders.php" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Orders"><i class="fa-solid fa-shipping-fast fa-lg"></i></a></li>
             <li><a href="?logout" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Sign out"><i class="fa-solid fa-sign-out-alt fa-lg"></i></a></li>
