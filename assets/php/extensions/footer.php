eee8aad589837393a0dcb0ebfaa2b8f9e25b3303
redpangilinan <redpangilinan715@gmail.com>
2023-04-16 17:43:14 +0800

    Create footer.php

---
+++ assets/php/extensions/footer.php
@@ -0,0 +1,12 @@
+<footer class="text-center text-white" style="background-color: #165853;">
+    <div class="container p-4 pb-0">
+        <section class="mb-4">
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/profile.php?id=100064229854714" role="button"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" href="https://mail.google.com/mail/u/0/?fs=1&to=im.ice31@gmail.com&su=&body=&tf=cm" role="button"><i class="fab fa-google"></i></a>
+        </section>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2021-<?php echo date("Y"); ?> Copyright.
+        <a class="text-white" href="https://www.facebook.com/profile.php?id=100064229854714">Andrea's Fresh and Greens</a>
+    </div>
+</footer>
