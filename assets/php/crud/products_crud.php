1f1e7eb88d81d51364d93f9f9656d06361581aa0
redpangilinan <redpangilinan715@gmail.com>
2023-04-16 17:25:37 +0800

    Added product status crud functionality and expense validation

--- assets/php/crud/products_crud.php
+++ assets/php/crud/products_crud.php
@@ -4,7 +4,7 @@ if (isset($_POST['input'])) {
     include '../connection.php';
     $input = mysqli_real_escape_string($conn, $_POST['input']);
     $sql =
-        "SELECT product_id, product, category, details, expense, price, image
+        "SELECT product_id, product, category, details, expense, price, status, image
     FROM tb_products
     WHERE (product_id LIKE '{$input}%'
     OR product LIKE '{$input}%'
@@ -28,6 +28,9 @@ if (isset($_POST['input'])) {
             <td><?php echo "₱" . $row["expense"] ?></td>
             <td><?php echo "₱" . $row["price"] ?></td>
             <td>
+                <button data-id="<?php echo $row["product_id"] ?>" class="availability btn btn-secondary" style="width: 7rem;"><?php echo $row["status"] ?></button>
+            </td>
+            <td>
                 <div class="btn-group" role="group" aria-label="modify">
                     <button data-id="<?php echo $row["product_id"] ?>" class="edit-data btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal"><i class="fas fa-edit"></i></button>
                     <button data-id="<?php echo $row["product_id"] ?>" class="delete-data btn btn-danger"><i class="fas fa-trash"></i></button>
@@ -39,7 +42,7 @@ if (isset($_POST['input'])) {
     if (($count) == 0) {
     ?>
         <tr>
-            <td colspan='8'>There are no records.</td>
+            <td colspan='9'>There are no records.</td>
         </tr>
 <?php
     }
@@ -65,13 +68,18 @@ if (
     $price = $_POST['price'];
 
     // Sanitize the input and insert the data into the database
-    $stmt = mysqli_prepare($conn, "INSERT INTO tb_products VALUES (null, ?, ?, ?, ?, ?, ?)");
-    mysqli_stmt_bind_param($stmt, "sssdds", $product, $category, $details, $expense, $price, $file_name);
-    if (mysqli_stmt_execute($stmt)) {
-        echo "success";
+    if ($expense >= $price) {
+        echo "expense_handler";
     } else {
-        echo "Error: " . mysqli_stmt_error($stmt);
+        $stmt = mysqli_prepare($conn, "INSERT INTO tb_products VALUES (null, ?, ?, ?, ?, ?, 'Available', ?)");
+        mysqli_stmt_bind_param($stmt, "sssdds", $product, $category, $details, $expense, $price, $file_name);
+        if (mysqli_stmt_execute($stmt)) {
+            echo "success";
+        } else {
+            echo "Error: " . mysqli_stmt_error($stmt);
+        }
     }
+
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 }
@@ -88,7 +96,7 @@ if (
     include '../connection.php';
     include '../image_edit.php';
 
-    // Sanitize the user input to before updating record
+    // Store input values into variables
     $primary_id = $_POST['primary_id'];
     $edit_product = $_POST['edit_product'];
     $edit_details = $_POST['edit_details'];
@@ -96,18 +104,24 @@ if (
     $edit_expense = $_POST['edit_expense'];
     $edit_price = $_POST['edit_price'];
 
-    if ($file_name == "default.jpg") {
-        $stmt = mysqli_prepare($conn, "UPDATE tb_products SET product=?, category=?, details=?, expense=?, price=? WHERE product_id=?");
-        mysqli_stmt_bind_param($stmt, "sssdds", $edit_product, $edit_category, $edit_details, $edit_expense, $edit_price, $primary_id);
+    // Sanitize the user input to before updating record
+    if ($expense >= $price) {
+        echo "expense_handler";
     } else {
-        $stmt = mysqli_prepare($conn, "UPDATE tb_products SET product=?, category=?, details=?, expense=?, price=?, image=? WHERE product_id=?");
-        mysqli_stmt_bind_param($stmt, "sssddss", $edit_product, $edit_category, $edit_details, $edit_expense, $edit_price, $file_name, $primary_id);
+        if ($file_name == "default.jpg") {
+            $stmt = mysqli_prepare($conn, "UPDATE tb_products SET product=?, category=?, details=?, expense=?, price=? WHERE product_id=?");
+            mysqli_stmt_bind_param($stmt, "sssdds", $edit_product, $edit_category, $edit_details, $edit_expense, $edit_price, $primary_id);
+        } else {
+            $stmt = mysqli_prepare($conn, "UPDATE tb_products SET product=?, category=?, details=?, expense=?, price=?, image=? WHERE product_id=?");
+            mysqli_stmt_bind_param($stmt, "sssddss", $edit_product, $edit_category, $edit_details, $edit_expense, $edit_price, $file_name, $primary_id);
+        }
+        if (mysqli_stmt_execute($stmt)) {
+            echo "success";
+        } else {
+            echo "Error: " . mysqli_stmt_error($stmt);
+        }
     }
-    if (mysqli_stmt_execute($stmt)) {
-        echo "success";
-    } else {
-        echo "Error: " . mysqli_stmt_error($stmt);
-    }
+
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 }
