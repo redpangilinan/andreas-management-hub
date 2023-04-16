8f5f6ac3ee4b534369821785f038ff747dde090d
redpangilinan <redpangilinan715@gmail.com>
2023-04-16 20:23:10 +0800

    Integrated best seller to the new ordering app design

--- assets/php/ordering/best_seller.php
+++ assets/php/ordering/best_seller.php
@@ -63,13 +63,47 @@ list($productNames, $productSold, $productImages) = soldProducts($data);
 array_multisort($productSold, SORT_DESC, $productNames);
 
 // Print the top 3 highest selling products
-$num_products = 3;
-for ($i = 0; $i < $num_products; $i++) {
-    $productImage = $productImages[$productNames[$i]];
-    echo "
-    <div class='best-seller'>
-        <img src='./assets/images/products/{$productImage}' alt='' class='best-img'>
-        <h6>{$productNames[$i]}</h6>
-        <p>Best seller</p>
-    </div>";
+function bestSeller($productNames, $productImages)
+{
+    $num_products = 3;
+    for ($i = 0; $i < $num_products; $i++) {
+        if ($i == 0) {
+            $best_class = "best-left";
+        } else if ($i == 1) {
+            $best_class = "best-mid";
+        } else {
+            $best_class = "best-right";
+        }
+        $productImage = $productImages[$productNames[$i]];
+        echo "
+        <div class='$best_class'>
+            <div class='best-img'><img src='./assets/images/products/{$productImage}' alt=''>
+                <h4>{$productNames[$i]}</h4>
+            </div>
+        </div>";
+    }
+}
+
+// Print the top 3 highest selling products for mobile
+function bestSellerM($productNames, $productImages)
+{
+    $num_products = 3;
+    for ($i = 0; $i < $num_products; $i++) {
+        if ($i == 0) {
+            $best_class = "carousel-item active";
+        } else {
+            $best_class = "carousel-item";
+        }
+        $productImage = $productImages[$productNames[$i]];
+        echo "
+        <div class='$best_class'>
+            <div class='best-mob'>
+                <div class='best-mob-img'><img src='./assets/images/products/{$productImage}' alt=''>
+                    <div class='carousel-caption d-block'>
+                        <h5>{$productNames[$i]}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>";
+    }
 }
