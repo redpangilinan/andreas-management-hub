2eb1fafcd4971b8c3cc1f927c231943ab2af88ff
redpangilinan <redpangilinan715@gmail.com>
2023-04-17 17:30:11 +0800

    Removed starting year

--- assets/php/extensions/footer.php
+++ assets/php/extensions/footer.php
@@ -6,7 +6,7 @@
         </section>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2021-<?php echo date("Y"); ?> Copyright.
+        © <?php echo date("Y"); ?> Copyright.
         <a class="text-white" href="https://www.facebook.com/profile.php?id=100064229854714">Andrea's Fresh and Greens</a>
     </div>
 </footer>
