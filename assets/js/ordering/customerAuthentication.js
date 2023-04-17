f63d774274039a54c87056f1713dfcf4003fca53
redpangilinan <redpangilinan715@gmail.com>
2023-04-17 23:46:17 +0800

    Enabled customer form functionality and added address validation with Nominatim API

--- assets/js/ordering/customerAuthentication.js
+++ assets/js/ordering/customerAuthentication.js
@@ -1,19 +1,62 @@
+const customerSubmit = document.querySelector("#customer-submit")
 const customerDetails = localStorage.getItem('customerDetails');
 
-const setCustomer = (firstName, lastName, contactNo, address) => {
-    const customerDetails = {
-        firstName: firstName,
-        lastName: lastName,
-        contactNo: contactNo,
-        address: address,
+const setCustomer = async (firstName, lastName, contactNo, address) => {
+    const query = encodeURI(address);
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=json`);
+        const data = await response.json();
+        if (data && data.length) {
+            const customerDetails = {
+                firstName: firstName,
+                lastName: lastName,
+                contactNo: contactNo,
+                address: address,
+            }
+            localStorage.setItem('customerDetails', JSON.stringify(customerDetails));
+            customAlert("success", "Success!", "Your details have been saved.");
+            customerInfoModal.hide();
+        } else {
+            customAlert("error", "Address not found!", "Please enter a proper address.");
+            customerInfoModal.hide();
+        }
+    } catch (error) {
+        localStorage.setItem('customerDetails', JSON.stringify(customerDetails));
+        customAlert("error", "API Error!", "An error occured while API is being called. Your details have been saved anyways.");
+        customerInfoModal.hide();
     }
-    localStorage.setItem('customerDetails', JSON.stringify(customerDetails));
 }
 
-function isAuthenticated() {
+const isAuthenticated = () => {
     if (localStorage.getItem('customerDetails') !== null) {
         return true;
     } else {
         return false;
     }
 }
+
+// Enables customer form to be submitted
+$("#customer-form").submit(function (e) {
+    e.preventDefault();
+    customerSubmit.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    Loading...`;
+    customerSubmit.disabled = true;
+
+    const form = document.getElementById('customer-form');
+    const firstName = form.firstname.value;
+    const lastName = form.lastname.value;
+    const address = form.address.value;
+    const contactNo = form.contact_no.value;
+
+    setCustomer(firstName, lastName, contactNo, address)
+        .then(() => {
+            console.log('Customer details saved successfully');
+            $("#customer-submit").html(`Save changes`);
+            customerSubmit.disabled = false;
+        })
+        .catch((error) => {
+            console.error('Error while saving customer details', error);
+            $("#customer-submit").html(`Save changes`);
+            customerSubmit.disabled = false;
+        });
+});
