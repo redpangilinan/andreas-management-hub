2b7ea410b3665a333abe414c3c9cc1f035a220f6
redpangilinan <redpangilinan715@gmail.com>
2023-05-20 20:55:33 +0800

    Optimized the update status to disable updating completed orders and disable updating an order back to approval when an order is already approved

--- assets/php/crud/update_order_status.php
+++ assets/php/crud/update_order_status.php
@@ -4,31 +4,32 @@ include '../connection.php';
 $primary_id = $_POST['primary_id'];
 $status = $_POST['status'];
 
-// Check the current status of the order
-$checkStmt = mysqli_prepare($conn, "SELECT status FROM tb_orders WHERE order_id = ?");
-mysqli_stmt_bind_param($checkStmt, 's', $primary_id);
-mysqli_stmt_execute($checkStmt);
-mysqli_stmt_bind_result($checkStmt, $currentStatus);
+// Retrieve the current status of the order_id
+$stmt = mysqli_prepare($conn, "SELECT status FROM tb_orders WHERE order_id = ?");
+mysqli_stmt_bind_param($stmt, "s", $primary_id);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $currentStatus);
+mysqli_stmt_fetch($stmt);
+mysqli_stmt_close($stmt);
 
-if (mysqli_stmt_fetch($checkStmt)) {
-    if ($currentStatus !== "Complete") {
-        // Update the status if it's not complete
-        $updateStmt = mysqli_prepare($conn, "UPDATE tb_orders SET status = ? WHERE order_id = ?");
-        mysqli_stmt_bind_param($updateStmt, "ss", $status, $primary_id);
-
-        if (mysqli_stmt_execute($updateStmt)) {
+// Check if the order exists and its current status is not 'Complete'
+if ($currentStatus && $currentStatus !== 'Complete') {
+    // Check if the new status is not 'Approval'
+    if ($status !== 'Approval') {
+        // Update the status in the database
+        $stmt = mysqli_prepare($conn, "UPDATE tb_orders SET status = ? WHERE order_id = ?");
+        mysqli_stmt_bind_param($stmt, "ss", $status, $primary_id);
+        if (mysqli_stmt_execute($stmt)) {
             echo "success";
         } else {
-            echo "Error: " . mysqli_stmt_error($updateStmt);
+            echo "Error: " . mysqli_stmt_error($stmt);
         }
-
-        mysqli_stmt_close($updateStmt);
+        mysqli_stmt_close($stmt);
     } else {
-        echo "order_complete_update";
+        echo "order_approved";
     }
 } else {
-    echo "Error: Order not found";
+    echo "order_complete_update";
 }
 
-mysqli_stmt_close($checkStmt);
 mysqli_close($conn);
